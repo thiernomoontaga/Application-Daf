f1266a507d3dd9358e032036805209f7f0235a58
Mary Vonne Diallo <jesuisetudiant10@gmail.com>
2025-07-21 16:11:44 +0000

    test2

--- src/entity/Logging.php
+++ src/entity/Logging.php
@@ -1,5 +1,67 @@
 <?php
 
-class logging{
-    
-}
+ namespace Src\Entity;
+
+use DateTime;
+
+class Citoyen{
+     
+   private ?DateTime $dateHeure;
+   private String $localisation;
+   private String $adresseIp;
+   private ?Statut $statut;
+   
+
+
+
+    public function __construct( ?DateTime $dateHeure=null ,$localisation='',$adresseIp='', ?Statut $statut=null){
+    $this->dateHeure = $dateHeure;
+    $this->localisation= $localisation;
+    $this->adresseIp = $adresseIp;
+    $this->statut = $statut;
+
+  }
+                public function getDateHeure(): ?DateTime
+                {
+                    return $this->dateHeure;
+                }
+
+                public function getLocalisation(): string
+                {
+                    return $this->localisation;
+                }
+
+                public function getAdresseIp(): string
+                {
+                    return $this->adresseIp;
+                }
+
+                public function getStatut(): ?Statut
+                {
+                    return $this->statut;
+                }
+
+                public function setDateHeure(?DateTime $dateHeure): self
+                {
+                    $this->dateHeure = $dateHeure;
+                    return $this;
+                }
+
+                public function setLocalisation(string $localisation): self
+                {
+                    $this->localisation = $localisation;
+                    return $this;
+                }
+
+                public function setAdresseIp(string $adresseIp): self
+                {
+                    $this->adresseIp = $adresseIp;
+                    return $this;
+                }
+
+                public function setStatut(?Statut $statut): self
+                {
+                    $this->statut = $statut;
+                    return $this;
+                }
+                }

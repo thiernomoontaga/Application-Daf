401c18fe954ba7da5fdc84944861968962cad070
AnnaSock <annasock542@gmail.com>
2025-07-21 18:29:47 +0000

    mise en place env.php et dossier bin

--- app/config/env.php
+++ app/config/env.php
@@ -5,3 +5,26 @@ use Dotenv\Dotenv;
 $dotenv = Dotenv::createUnsafeImmutable(dirname(__DIR__,2));
 $dotenv->load();
 
+
+define('BASE_DSN_POSTGRES', $_ENV['BASE_DSN_POSTGRES']);
+define('DB_USER_POSTGRES', $_ENV['DB_USER_POSTGRES']);
+define('DB_PASS_POSTGRES', $_ENV['DB_PASS_POSTGRES']);
+
+
+
+define('DB_DRIVER', $_ENV['DB_DRIVER']);
+
+
+
+define('BASE_DSN_MYSQl', $_ENV['BASE_DSN_MYSQl']);
+define('DB_USER_MYSQL', $_ENV['DB_USER_MYSQL']);
+define('DB_PASS_MYSQL', $_ENV['DB_PASS_MYSQL']);
+
+
+
+
+
+
+
+
+

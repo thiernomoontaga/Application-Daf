fbc1cfcc8ce6e4616ad139f8cf14c689727af8ba
Thierno <thiernosegnane316@gmail.com>
2025-07-23 12:30:46 +0000

    mise en place de la classe singleton

---
+++ app/config/Singleton.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Config;
+
+class Singleton{
+  protected static ?Singleton $instance = null;
+  private function __construct()
+  {
+    
+  }
+  private static function getInstance(){
+    if(self::$instance === null){
+      self::$instance = new self();
+    }
+    return self::$instance;
+  }
+}
--- src/repository/CitoyenRepository.php
+++ src/repository/CitoyenRepository.php
@@ -44,4 +44,3 @@ class CitoyenRepository implements ICitoyenRepository
     }
 
     }
-}
